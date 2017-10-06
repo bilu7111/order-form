7f4cc2e97c194d1d7c9683648c469568d425bc87
AQHawk <ironhawkcreations@gmail.com>
2017-10-05 18:30:44 -0700

    Almost there

---
+++ list.js
@@ -0,0 +1,32 @@
+'use strict';
+
+localStorage.formData = document.getElementById('ordered');
+
+// new Busmall ('Bag', 'img/bag.jpg', 'bag');
+// new Busmall ('Banana', 'img/banana.jpg', 'banana');
+// new Busmall ('Bathroom', 'img/bathroom.jpg', 'bathroom');
+// new Busmall ('Boots', 'img/boots.jpg', 'boots');
+// new Busmall ('Breakfast', 'img/breakfast.jpg', 'breakfast');
+// new Busmall ('Bubblegum', 'img/bubblegum.jpg', 'bubblegum');
+// new Busmall ('Chair', 'img/chair.jpg', 'chair');
+// new Busmall ('Cthulhu', 'img/cthulhu.jpg','cthulhu');
+// new Busmall ('Dogduck', 'img/dog-duck.jpg', 'dogduck');
+// new Busmall ('Dragon', 'img/dragon.jpg', 'dragon');
+// new Busmall ('Pen', 'img/pen.jpg', 'pen');
+// new Busmall ('Petsweep', 'img/pet-sweep.jpg', 'petsweep');
+// new Busmall ('Scissors', 'img/scissors.jpg', 'scissors');
+// new Busmall ('Shark', 'img/shark.jpg', 'shark');
+// new Busmall ('Sweep', 'img/sweep.png', 'sweep');
+// new Busmall ('Tauntaun', 'img/tauntaun.jpg', 'tauntaun');
+// new Busmall ('Unicorn', 'img/unicorn.jpg','unicorn');
+// new Busmall ('Usb', 'img/usb.gif', 'usb');
+// new Busmall ('Watercan', 'img/water-can.jpg', 'watercan');
+// new Busmall ('Wineglass', 'img/wine-glass.jpg', 'wineglass');
+
+
+var stuff =
+  localStorage.formData = JSON.parse(formData);
+  console.log(localStorage.formData);
+
+  //   // text content thing here
+  //   // concate image soure and append
